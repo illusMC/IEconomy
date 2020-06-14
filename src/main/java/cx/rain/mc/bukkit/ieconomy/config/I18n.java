9759b5814831c43da6a9ccf755cc47f5a8a53fa7
秋雨落 <i@rain.cx>
2020-06-14 16:49:58 +0800

    change: 我怒斥写代码该空格不空格，该回车不回车的！

--- src/main/java/cx/rain/mc/bukkit/ieconomy/config/I18n.java
+++ src/main/java/cx/rain/mc/bukkit/ieconomy/config/I18n.java
@@ -4,23 +4,24 @@ import cc.sfclub.util.common.JsonConfig;
 import lombok.Setter;
 
 public class I18n extends JsonConfig {
-    private String locale="en_US";
+    private String locale = "en_US";
     @Setter
     private static I18n inst;
 
-    public I18n(String rootDir,String locale) {
+    public I18n(String rootDir, String locale) {
         super(rootDir);
-        this.locale=locale;
+        this.locale = locale;
     }
 
     @Override
     public String getConfigName() {
-        return locale+".json";
+        return locale + ".json";
     }
 
     public static I18n get(){
         return inst;
     }
 
-    public String new_version_published="Version %v released on %date.\nUpdate it: https://github.com/illusMC/IEconomy/releases";
+    public String new_version_published =
+            "Version %v released on %date.\nUpdate it: https://github.com/illusMC/IEconomy/releases";
 }
