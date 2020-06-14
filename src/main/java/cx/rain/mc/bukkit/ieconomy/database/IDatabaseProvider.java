65f69c4fd8161785b1a25316efc104763e38e219
秋雨落 <i@rain.cx>
2020-06-14 11:34:41 +0800

    临时提交……java连数据库太难了
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/cx/rain/mc/bukkit/ieconomy/database/IDatabaseProvider.java
@@ -0,0 +1,5 @@
+package cx.rain.mc.bukkit.ieconomy.database;
+
+public interface IDatabaseProvider {
+    IQueryable fetch();
+}
---
+++ src/main/java/cx/rain/mc/bukkit/ieconomy/database/implement/MySql.java
@@ -0,0 +1,6 @@
+package cx.rain.mc.bukkit.ieconomy.database.implement;
+
+import cx.rain.mc.bukkit.ieconomy.database.IDatabaseProvider;
+
+public class MySql implements IDatabaseProvider {
+}
